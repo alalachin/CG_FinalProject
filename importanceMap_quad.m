3f22569f923db049408d6b3a951f3a8f80f71122
alalachin <105703050@nccu.edu.tw>
2020-09-28 15:18:10 +0800

    Update importanceMap_quad.m

--- importanceMap_quad.m
+++ importanceMap_quad.m
@@ -1,16 +1,4 @@
 function [ importance_quad] = SNS_importanceMap_quad(importance_map, Vertex_set_org)
-% Summary of this function goes here
-%   Detailed explanation goes here
-% The Code (Version 1) is created by ZHANG Yabin,
-% Nanyang Technological University, 2015-12-30
-% which is based on the method described in the following paper 
-% [1] Wang, Yu-Shuen, et al. "Optimized scale-and-stretch for image resizing." 
-% ACM Transactions on Graphics (TOG) 27.5 (2008): 118. 
-% The binary code is provided on the project page:
-% http://graphics.csie.ncku.edu.tw/Image_Resizing/
-% The Matlab codes are for non-comercial use only.
-% Note that the importance maps are slightly different from the original
-% ones, and the retargeted images are influenced.
 
     quad_num_h = size(Vertex_set_org,1) -1;
     quad_num_w = size(Vertex_set_org,2) -1;
