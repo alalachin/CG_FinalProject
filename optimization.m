310ef49090ca630758abb45067db6da05322aa60
alalachin <105703050@nccu.edu.tw>
2020-09-28 15:16:55 +0800

    Update optimization.m

--- optimization.m
+++ optimization.m
@@ -1,15 +1,4 @@
 function [ Vertex_updated ] = SNS_optimization(Vertex_set_org, Vertex_warped_initial, importance_quad)
-% Summary of this function goes here
-%   Detailed explanation goes here% The Code (Version 1) is created by ZHANG Yabin,
-% Nanyang Technological University, 2015-12-30
-% which is based on the method described in the following paper 
-% [1] Wang, Yu-Shuen, et al. "Optimized scale-and-stretch for image resizing." 
-% ACM Transactions on Graphics (TOG) 27.5 (2008): 118. 
-% The binary code is provided on the project page:
-% http://graphics.csie.ncku.edu.tw/Image_Resizing/
-% The Matlab codes are for non-comercial use only.
-% Note that the importance maps are slightly different from the original
-% ones, and the retargeted images are influenced.
 
 [Vertex_updated] = ...
     SNS_Opt_Iter_M(Vertex_set_org ,Vertex_warped_initial, importance_quad);
